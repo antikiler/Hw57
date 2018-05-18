bbb243a83a7031b1b108e8972c7de98ec01ac03a
Maksim Limarev <limarevqw@gmail.com>
2018-05-18 14:17:01 +0600

    first commit

---
+++ src/AppBundle/Controller/UserController.php
@@ -0,0 +1,16 @@
+<?php
+namespace AppBundle\Controller;
+use AppBundle\Entity\User;
+use AppBundle\Form\UserType;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+/**
+ * Class UserController
+ * @package AppBundle\Controller
+ * @Route("user")
+ */
+class UserController extends Controller {
+
+}
---
+++ src/AppBundle/Form/UserType.php
@@ -0,0 +1,33 @@
+<?php
+namespace AppBundle\Form;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name',TextType::class,[
+                'label'=>"Ваше имя:"
+            ])
+            ->add('email', EmailType::class,[
+                'label'=>"E-mail:"
+            ])
+            ->add('password', PasswordType::class,[
+                'label'=>'Ваш пароль:'
+            ])
+            ->add('save', SubmitType::class, ['label' => 'Регистрация']);
+    }
+    public function configureOptions(OptionsResolver $resolver)
+    {
+    }
+    public function getBlockPrefix()
+    {
+        return 'app_bundle_user_type';
+    }
+}
